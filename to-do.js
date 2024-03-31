9ef83279925448ec810be1fdb353efb3b15a2f50
willboyle18 <90668012+willboyle18@users.noreply.github.com>
2024-03-31 14:45:18 -0700

    Implement Add and Complete Functionality
    Added ability to add tasks to the list as well as remove them by pushing a button attached to each task

--- to-do.js
+++ to-do.js
@@ -1,4 +1,47 @@
+
+
 function addNewItem(){
+    // Get references to the input field and the submit button
+    // This code is only executed once on start-up and it sets up the asynchronous event listeners
+    console.log("Function Call");
+    const inputField = document.getElementById('userInput');
+    const addButton = document.getElementById('addButton');
+
+    // Asynchronous event listener for adding a task to the list
+    addButton.addEventListener('click', function(){
+        console.log("Adding task to list");
+        if(inputField.value != ''){
+            const task = inputField.value; // Extracts the text from the input field
+            var newItem = document.createElement("li"); // Creates a new list element to insert into the list
+            newItem.textContent = task; // Uses the extracted text for the task
+
+            var completeButton = document.createElement("button"); // Create the complete task button
+            completeButton.textContent = "Complete Task"; // Add text to the complete task button
+            completeButton.style.marginLeft = "10px"; // Directly adds styling with DOM manipulation but CSS is recommended
+            newItem.appendChild(completeButton); // Adds the complete button element to the new task
+
+            var list = document.getElementById("dynamicList");
+            list.appendChild(newItem); // Adds the task to the list
+            console.log("Task added to list")
+
+            // Asynchronous event listener for removing a task from the list
+            completeButton.addEventListener('click', function() {
+                newItem.remove(); // Removes 
+                console.log("Task complete");
+            });
+        }
+        else{
+            alert('Please enter a task');
+            console.log('Task failed to add');
+        }
+        
+        inputField.value = '';
+        console.log("Input field cleared");
+    });
+
+    
+
+    /*
     // Create a new list item element
     var newItem = document.createElement("li");
 
@@ -10,7 +53,8 @@ function addNewItem(){
 
     // Append the new list item to the list
     list.appendChild(newItem);
+    */
 }
 
-addNewItem();
-console.log("Function Called");
+
+window.onload = addNewItem;
